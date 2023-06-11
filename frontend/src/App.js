31476fc62ef1c99e3b99390b3b6fd33f84c84ee0
unknown <hexvexdeveloper@gmail.com>
2023-06-10 23:52:56 -0700

    Added navbar with logo to webpage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { AppProvider } from "./context/AppContext";
+import carlogo from "./images/carlogo.jpg";
 import './App.css';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav class="navbar navbar-dark bg-dark">
+      <ul>
+            <img src={carlogo} width="30" height="30" class="d-inline-block align-top" alt=""></img>
+            <a href="#" className="navbar-brand">Car Calculator</a>
+      </ul>
+    </nav>
   );
 }
 
